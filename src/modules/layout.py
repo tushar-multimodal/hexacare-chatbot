20aee9ad2fb1f265958b66f62cde80d9789e32be
tushar-multimodal <140727475+tushar-multimodal@users.noreply.github.com>
2023-08-01 10:39:23 +0530

    Update layout.py

--- src/modules/layout.py
+++ src/modules/layout.py
@@ -33,7 +33,7 @@ class Layout:
         with st.form(key="my_form", clear_on_submit=True):
             user_input = st.text_area(
                 "Query:",
-                placeholder="Ask me anything about the document...",
+                placeholder="Ask me anything abouy Hexaware...",
                 key="input",
                 label_visibility="collapsed",
             )
